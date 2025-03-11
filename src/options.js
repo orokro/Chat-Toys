4e1b11749ea2a5092d9673161d1964c974fdf0a9
Greg Miller <hypurban86@gmail.com>
2025-03-10 20:01:27 -0400

    formatting.

--- src/options.js
+++ src/options.js
@@ -4,39 +4,39 @@ let colorChangeTimer = null;
 function saveCurrentColor() {
 
 	const colorPicker = document.getElementById("colorPicker");
-		const newColor = colorPicker.value;
-		chrome.storage.sync.set({ bgColor: newColor });
+	const newColor = colorPicker.value;
+	chrome.storage.sync.set({ bgColor: newColor });
 
 }
 
 
 function scheduleColorChange() {
-	
-	if(colorChangeTimer!=null)
+
+	if (colorChangeTimer != null)
 		return;
 
 	saveCurrentColor();
 
 	colorChangeTimer = setTimeout(() => {
 
-		saveCurrentColor();	
+		saveCurrentColor();
 		colorChangeTimer = null;
 	}
-	, 300);
+		, 300);
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
 	const colorPicker = document.getElementById("colorPicker");
-	
+
 	chrome.storage.sync.get("bgColor", ({ bgColor }) => {
 		if (bgColor) {
 			colorPicker.value = bgColor;
 		}
 	});
-	
+
 	colorPicker.addEventListener("input", (event) => {
 		scheduleColorChange();
 	});
-	
+
 });
