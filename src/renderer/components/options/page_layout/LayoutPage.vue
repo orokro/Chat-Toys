dc8bdcc68d7164549879e9212f1002282c0916ca
Greg Miller <hypurban86@gmail.com>
2025-04-27 17:50:57 -0400

    Adds the live link to the lay out page & cleans up layout page a bit.

--- src/renderer/components/options/page_layout/LayoutPage.vue
+++ src/renderer/components/options/page_layout/LayoutPage.vue
@@ -21,18 +21,44 @@
 			<!-- the main money -->
 			<LayoutScreen :activeTab="selectedTab" />
 
+			<SectionHeader title="Single Page URL"/>
+			<SettingsRow>
+				<p>
+					As described below, this URL can be loaded into an OBS Browser source
+					as a way to layout multiple toy widgets in a single browser source.
+				</p>
+				<p>
+					You can also copy the URL below and load it in your Web Browser to test the layout.
+					The layout should reflect what you configured in the screen above.
+				</p>
+				<URLCopyBox :url="livePageURL" />
+			</SettingsRow>
+
 			<SectionHeader title="About Layout Configuration"/>
 			<p>
-				Above you will find a screen full of widgets for the Chat Toy's you've enabled
-				and configured in the Toy Box tab.
+				When incorporating the Chat Toys widgets into your stream, you have two options:
+			</p>
+			<ul>
+				<li><strong>Preferred Method:</strong> Use the Widget URLs provided on the Toy pages directly as browser sources</li>
+				<li><strong>Single Source Method:</strong> Use this page to layout all the widgets on a single OBS Browser Source</li>
+			</ul>
+			<p>
+				<br/>
+				Using each widget as a separate browser source is the preferred method, as it allows for more flexibility in OBS,
+				as you can lay them out using OBS tools, and you can have different widgets for different scenes.
 			</p>
 			<p>
-				The screen is not to scale, but the aspect ratio is correct based on the resolution
-				of the stage you've set in the settings above the screen box. Note that, this is not where you capture,
-				a separate popup window will be used for that.
+				However, using multiple browser sources for each widget may affect performance on some systems.
+				This page allows you to lay out all your widgets, and load them in OBS with a single Browser source.
+				This may work better in OBS, but the lay out controls are limited, and every scene will have the same layout.
 			</p>
 			<p>
-				This screen is simply for laying out the widgets.
+				You can use this page to configure the layout for the single source.<br> <strong><em>If you don't plan on using
+				the single source, you don't need to use this layout system at all.</em></strong>
+			</p>
+			<p>
+				The above screen is not to scale, but the aspect ratio is correct based on the resolution
+				of the stage you've set in the settings above the screen box.
 			</p>
 			<p>
 				Click the tabs on the left to enable-editing of a specific toy's widgets.
@@ -68,7 +94,24 @@
 				<br>
 				Therefore, you can uncheck "Show All Widgets" to see only the item you're working on.
 			</p>
-
+			
+			<SectionHeader title="Widget Demo Mode"/>
+			<SettingsInputRow
+				type="boolean"
+				v-model="ctApp.demoMode.value"
+			>
+				<h3>Widget Demo Mode</h3>
+				<p>When enabled, the various Chat Toy's Widgets will display in "<strong>demo mode</strong>".</p>
+				<p>
+					This can help you adjust your layout in OBS or on this screen.
+					For example, some components like Media Items, or the Prize wheel will only appear when
+					they are activated by the chatters. With <strong>demo mode</strong> enabled, they will
+					be visible so you can see how they look on screen.
+				</p>
+				<p>
+					<strong>NOTE:</strong> make sure you disable <strong>demo mode</strong> before you go live!
+				</p>
+			</SettingsInputRow>
 		</PageBox>
 	</VerticalItemsPage>
 
@@ -83,6 +126,8 @@ import VerticalItemsPage from '../VerticalItemsPage.vue';
 import LayoutScreen from './LayoutScreen.vue';
 import PageBox from '../PageBox.vue';
 import SectionHeader from '../SectionHeader.vue';
+import SettingsInputRow from '../SettingsInputRow.vue';
+import URLCopyBox from '../URLCopyBox.vue';
 
 // fetch the main app state context
 const ctApp = inject('ctApp');
@@ -95,6 +140,18 @@ const verticalItems = computed(() => {
 
 // what's the selected tab?
 const selectedTab = ref(verticalItems.value[0]?.slug || '');
+
+// figure out which URL to show for the test page based on our mode
+const livePageURL = computed(() => {
+	
+	if (window.env.isDev) {
+		return 'http://localhost:8080/live.html';
+	} else {
+		const port = ctApp.serverPort.value;
+		return `http://localhost:${port}/live/`;
+	}
+});
+
 
 </script>
 <style lang="scss" scoped>
