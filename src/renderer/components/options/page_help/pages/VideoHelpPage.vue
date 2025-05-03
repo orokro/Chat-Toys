f0c016cad9d32aff95dd4c8ce48a417e96ca256b
Greg Miller <hypurban86@gmail.com>
2025-05-03 19:28:09 -0400

    implements the video help page

--- src/renderer/components/options/page_help/pages/VideoHelpPage.vue
+++ src/renderer/components/options/page_help/pages/VideoHelpPage.vue
@@ -12,20 +12,119 @@
 		themeImage="assets/bg_tiles/videos.png"
 	>
 		<p>
-			Welcome to the ToyBox plugin! This plugin is designed to help you interact with your viewers in fun and engaging ways.
+			<br/>
+			All the videos below are also available on the pages where they're relevant, 
+			<br/>
+			but if you're in a learning mood - you can peruse them all here!
 		</p>
 
-		<SectionHeader title="Important Video"/>
-		<iframe 
-			width="560"
-			height="315"
-			src="https://www.youtube.com/embed/iR3jYHwa6OM?si=c4184fgy297NiD8l"
-			sandbox="allow-scripts allow-same-origin"
-			allowfullscreen
-		></iframe>
+		<SectionHeader title="Chat Toys Video Help Library"/>
 
-		<SectionHeader title="CatsumIpsum"/>
-		<CatsumIpsum :paragraphs="5" :sentences="10"/>
+		<!-- OBS Settings -->
+		<YTVideoBox 
+			url="https://youtu.be/XyfmKtksFIg"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Chat Settings -->
+		<YTVideoBox 
+			url="https://youtu.be/hd7dndoe8X4"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Assets Database -->
+		<YTVideoBox 
+			url="https://youtu.be/x8Pf10d15BM"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Users Database -->
+		<YTVideoBox 
+			url="https://youtu.be/bWa-IjAIPfI"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Layout Page -->
+		<YTVideoBox 
+			url="https://youtu.be/sJOo2MQIhjg"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Channel Points Page -->
+		<YTVideoBox 
+			url="https://youtu.be/wGxNc5xE-ww"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Output Log Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/fC1Z2l9_rfQ"
+			width="100%"
+		/>
+		<br/>
+		
+		<!-- Chat Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/kc-181dg2M8"
+			width="100%"
+		/>
+		<br/>
+		
+		<!-- Fishing Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/sm5-RHsqQmI"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Gamba Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/lv5cDifVbTc"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Head Pats Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/wDCzZFhiU-s"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Media Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/35cJEtJNiq8"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Prize Wheel Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/JMofM8hf9SI"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Stream Buddies Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/7cNTiqa_Kq8"
+			width="100%"
+		/>
+		<br/>
+
+		<!-- Tosser Toy -->
+		<YTVideoBox 
+			url="https://youtu.be/B4YfacihXjc"
+			width="100%"
+		/>
+		<br/>
+		
 	</PageBox>
 
 </template>
@@ -39,6 +138,8 @@ import PageBox from '../../PageBox.vue';
 import SectionHeader from '../../SectionHeader.vue';
 import InfoBox from '../../InfoBox.vue';
 import CatsumIpsum from '../../../CatsumIpsum.vue';
+import YTVideoBox from '@components/YTVideoBox.vue';
+
 
 </script>
 <style lang="scss" scoped>	
