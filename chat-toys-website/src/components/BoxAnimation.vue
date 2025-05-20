ea795f8a206518436a7688463bd9015321afd74e
Greg Miller <hypurban86@gmail.com>
2025-05-20 17:12:50 -0400

    Begining of new animation system.

---
+++ chat-toys-website/src/components/BoxAnimation.vue
@@ -0,0 +1,51 @@
+<!--
+	BoxAnimation.vue
+	----------------
+
+	The main component that handles the logic for the opening of the box animation and
+	the various pieces that requires.
+
+	In short, we'll have the following pieces in z-order:
+	 - Box animation frames a square that shows the box rotating
+	 - Content div - where the content from our slot lives
+	 - Box-front - an image with the same size as the animation frames, that occludes the content
+	 - Box-lid - the png that scrolls up after the animation is over, making the box appear to open
+
+	In addition, we'll also have a area to display debug information, because
+	there's lots of moving parts making this responsive, so we'll show some info.
+-->
+<template>
+
+	<!-- displays debug info here -->
+	<div class="debug-info">
+		debug
+	</div>
+
+</template>
+<script setup>
+
+// vue
+import { ref, onMounted, computed } from 'vue';
+
+</script>
+<style lang="scss" scoped>
+
+	// fixed on top-left to show debug info
+	.debug-info {
+		
+		// fixed on top-left
+		position: fixed;
+		top: 0px;
+		left: 0px;
+
+		// dark mode transparent box with some spacing
+		background: rgba(0, 0, 0, .5);
+		padding: 10px;
+
+		// text-settings
+		color: white;
+		font-family: 'Courier New', Courier, monospace;
+		
+	}// .debug-info
+
+</style>
--- chat-toys-website/src/components/MainPage.vue
+++ chat-toys-website/src/components/MainPage.vue
@@ -2,48 +2,26 @@
 	MainPage.vue
 	------------
 
-	The main scrolling & ineractive page of the website
+	The main scrolling & interactive page of the website
 -->
 <template>
 
 	<!-- this thing will waste space for the beginning of the scrolling -->
 	<div class="top-scroll-space"></div>
 
-	<!-- header bar & image -->
-	<div 
-		:class="{
-			'header-bar': true,
-			'show': scrollY > 1480,
-		}"
-		align="center"
-	>
-		<!-- header image -->
-		<img
-			class="header-image"
-			src="../assets/img/header.png"
-			alt="header image"		
-		/>
-	</div>
-
-	<!-- the animated box segment -->
-	<BoxAni
-		class="box-ani"
-		:scroll-y="scrollY"
-		@box-open-t="handleBoxOpenStatusChange"
-	/>
-
-	<div class="box-content">
+	<!-- <div class="box-content">
 		Jump on fridge leave fur on owners clothes meoooow demand to be let outside at once, and expect owner to wait for me as i think about it chase little red dot someday it will be mine!. Why must they do that i cry and cry and cry unless you pet me, and then maybe i cry just for fun waffles hate dogs for humans,humans, humans oh how much they love us felines we are the center of attention they feed, they clean or the best thing in the universe is a cardboard box there's a forty year old lady there let us feast. Lay on arms while you're using the keyboard sit in a box for hours but stare at imaginary bug flop over, for ask for petting. I show my fluffy belly but it's a trap! if you pet it i will tear up your hand ask for petting yet swat at dog, and groom forever, stretch tongue and leave it slightly out, blep or why dog in house? i'm the sole ruler of this home and its inhabitants smelly, stupid dogs, inferior furballs time for night-hunt, human freakout. Go crazy with excitement when plates are clanked together signalling the arrival of cat food intently stare at the same spot, but cough hairball on conveniently placed pants. Slap owner's face at 5am until human fills food dish favor packaging over toy. Sit in a box for hours trip on catnip so plop down in the middle where everybody walks. Attack the dog then pretend like nothing happened destroy couch wake up human for food at 4am claws in your leg poop on grasses but look at dog hiiiiiisssss gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye. Stick butt in face. Hiiiiiiiiii feed me now the best thing in the universe is a cardboard box sleep all day whilst slave is at work, play all night whilst slave is sleeping, yet scream for no reason at 4 am cats go for world domination. Attack like a vicious monster mmmmmmmmmeeeeeeeeooooooooowwwwwwww or lie in the sink all day roll over and sun my belly, cat ass trophy pet me pet me don't pet me. Meow meow lick the curtain just to be annoying. Catch eat throw up catch eat throw up bad birds mouse scratch at door to be let outside, get let out then scratch at door immmediately after to be let back in stare out cat door then go back inside. Play with twist ties curl up and sleep on the freshly laundered towels and purr purr purr until owner pets why owner not pet me hiss scratch meow or sit as close as possible to warm fire without sitting on cold floor. Thinking longingly about tuna brine bite plants scream at teh bath hack up furballs. I show my fluffy belly but it's a trap! if you pet it i will tear up your hand kitty kitty pussy cat doll touch my tail, i shred your hand purrrr.
 
 		Chew iPad power cord car rides are evil brown cats with pink ears yet meowwww so meow loudly just to annoy owners what the heck just happened, something feels fishy or cereal boxes make for five star accommodation . Bite plants purr for lie on your belly and purr when you are asleep. Whatever purr while eating scratch my tummy actually i hate you now fight me. Be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day demand to have some of whatever the human is cooking, then sniff the offering and walk away. Bring your owner a dead bird somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock.
 
 		Swat at dog haha you hold me hooman i scratch, or put butt in owner's face scratch at door to be let outside, get let out then scratch at door immmediately after to be let back in plays league of legends. Play riveting piece on synthesizer keyboard i hate cucumber pls dont throw it at me, so you're just gonna scroll by without saying meowdy? and attack like a vicious monster. Cats woo. Let me in let me out let me in let me out let me in let me out who broke this door anyway kitty kitty pussy cat doll for i see a bird i stare at it i meow at it i do a wiggle come here birdy for lick the other cats. Stare at ceiling skid on floor, crash into wall meow meow you are my owner so here is a dead rat stretch out on bed, meowwww wack the mini furry mouse. Missing until dinner time then cats take over the world or cats are cute mew mew. Kitty poochy stretch out on bed. Get scared by doggo also cucumerro jump around on couch, meow constantly until given food, so hopped up on catnip wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again experiences short bursts of poo-phoria after going to the loo. Jump off balcony, onto stranger's head hey! you there, with the hands but make plans to dominate world and then take a nap roll on the floor purring your whiskers off loved it, hated it, loved it, hated it nyaa nyaa lies down . Cats go for world domination i heard this rumor where the humans are our owners, pfft, what do they know?!, eat fish on floor, so lick arm hair. Toy mouse squeak roll over to pet a cat, rub its belly, endure blood and agony, quietly weep, keep rubbing belly somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock yet chase red laser dot for snuggles up to shoulders or knees and purrs you to sleep sleeping in the box. I hate cucumber pls dont throw it at me. Make meme, make cute face found somthing move i bite it tail. What a cat-ass-trophy! meow and walk away. Lick plastic bags good morning sunshine hide at bottom of staircase to trip human. Sleep meowing non stop for food throw down all the stuff in the kitchen sweet beast always ensure to lay down in such a manner that tail can lightly brush human's nose touch my tail, i shred your hand purrrr mrow. Kitty scratches couch bad kitty cereal boxes make for five star accommodation take a big fluffing crap ðŸ’© but sit by the fire or what the heck just happened, something feels fishy. Cattt catt cattty cat being a cat purr when give birth. Sleep over your phone and make cute snoring noises. Loved it, hated it, loved it, hated it meow. Whatever cattt catt cattty cat being a cat mesmerizing birds sleep everywhere, but not in my bed get suspicious of own shadow then go play with toilette paper. Sit in window and stare oooh, a bird, yum attack the dog then pretend like nothing happened. Purr purr purr until owner pets why owner not pet me hiss scratch meow cat milk copy park pee walk owner escape bored tired cage droppings sick vet vomit thug cat , or meow meow. Lick the other cats give me some of your food give me some of your food give me some of your food meh, i don't want it.
 
-	</div>
+	</div> -->
 
-	<BoxFront
-		v-if="boxIsOpen"
-	/>
+	<BoxAnimation>
+
+		foo
+	</BoxAnimation>
 
 
 </template>
@@ -52,9 +30,10 @@
 // vue
 import { ref, onMounted, onUnmounted } from 'vue';
 
-// components
-import BoxAni from './BoxAni.vue';
-import BoxFront from './BoxFront.vue';
+// // components
+import BoxAnimation from './BoxAnimation.vue';
+// import BoxAni from './OLD/BoxAni.vue';
+// import BoxFront from './OLD/BoxFront.vue';
 
 // reactive vertical scroll variable
 const scrollY = ref(0)
