80a56a2d490204b457c311aa832bce4bb66de199
Greg Miller <hypurban86@gmail.com>
2026-06-03 03:13:44 -0400

    adds new icons

--- src/renderer/toys/Danmaku/DanmakuPage.vue
+++ src/renderer/toys/Danmaku/DanmakuPage.vue
@@ -10,7 +10,7 @@
 	<PageBox
 		title="Danmaku Comments Settings"
 		:themeColor="toy.static.themeColor"
-		themeImage="assets/bg_tiles/chat.png"
+		themeImage="assets/bg_tiles/danmaku.png"
 		bgThemePos="32px"
 	>
 
